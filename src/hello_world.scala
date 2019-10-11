27a2813dda6f1f048a5561f978dc3cbc92314fae
Christopher Chan <christopher.chan2014@gmail.com>
2019-10-10 22:06:30 -0400

    Restarting project
    Deleted existing files and replaced with sbt based project.
    Better aligned with tutorials and intelij recommendations.

--- src/hello_world.scala
+++
@@ -1,8 +0,0 @@
-object hello_world extends App{
-  println("Hello, world")
-}
-
-
-// https://learnxinyminutes.com/docs/scala/
-// https://docs.scala-lang.org/tutorials/tour/basics.html.html
-// https://www.scala-lang.org/old/sites/default/files/linuxsoft_archives/docu/files/ScalaByExample.pdf
