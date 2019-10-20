14c84e5f1b216eb20b542e6b1d549d76e19742c3
Christopher Chan <christopher.chan2014@gmail.com>
2019-10-19 23:50:30 -0400

    day2

---
+++ src/main/scala/mlewis/day2.scala
@@ -0,0 +1,16 @@
+package mlewis
+
+class day2 {
+  var i = 0
+  while (i < 10) {
+    println(i)
+    i += 1
+  }
+}
+
+object test {
+  def main(args: Array[String]): Unit = {
+    val thirdDay = new day2
+
+  }
+}
